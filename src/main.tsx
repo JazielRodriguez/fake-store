a65fef5371cb1e58c7677e37b4be53b9970d5cad
Jaziel Rodriguez <aguafapolla@gmail.com>
2022-05-25 18:03:22 -0500

    react lazy

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,25 @@ import App from './App'
 import './index.css'
 
 import { Route } from 'wouter'
-import Product from './pages/Product'
-import PageCategories from './pages/PageCategories'
-import SignIn from './pages/SignIn'
+const Product = React.lazy(() => import('./pages/Product'))
+// import Product from './pages/Product'
+const PageCategories = React.lazy(() => import('./pages/PageCategories'))
+// import PageCategories from './pages/PageCategories'
+const SignIn = React.lazy(() => import('./pages/SignIn'))
+// import SignIn from './pages/SignIn'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Route path='/'>
       <App />
     </Route>
-    <Route path='/signin'><SignIn /></Route>
-    <Route path='/products/product/:id'>{(params) => <Product id={params.id} />}</Route>
+    <Route path='/signin'>
+      <SignIn />
+    </Route>
+    <Route path='/products/product/:id'>
+      {(params) => <Product id={params.id} />}
+    </Route>
     <Route path='/products/:category'>
       {(params) => <PageCategories category={params.category} />}
     </Route>
-
   </React.StrictMode>
 )
